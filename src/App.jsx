483af8b314f9993d094b52393839700ef5d85441
Matheus Biesdorf <matheusbiesdorf@hotmail.com>
2025-01-20 00:26:25 -0300

    Banner

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,32 @@ import { styled } from "styled-components";
 import EstilosGlobais from "./componentes/estilos globais";
 import Cabecalho from "./componentes/Cabecalho";
 import BarraLateral from "./componentes/BarraLateral";
+import Banner from "./componentes/Banner";
+import bannerBackground from './assets/banner.png'
 
 const FundoGradiente = styled.div`
-   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+   background: linear-gradient(
+      174.61deg,
+      #041833 4.16%,
+      #04244f 48%,
+      #154580 96.76%
+   );
    width: 100%;
    min-height: 100vh;
-`
+`;
 
 function App() {
-   return(
-   <FundoGradiente>
-      <EstilosGlobais/>
-      <Cabecalho/>
-      <BarraLateral/>
-   </FundoGradiente>
-   )
+   return (
+      <FundoGradiente>
+         <EstilosGlobais />
+         <Cabecalho />
+         <BarraLateral />
+         <Banner
+            texto="A galeria mais completa de fotos do espaço!"
+            backgroundImage={bannerBackground}
+         />
+      </FundoGradiente>
+   );
 }
 
 export default App;
