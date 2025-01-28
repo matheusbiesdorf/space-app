f94026f06c10f6dbb12c2b5c9efffb3a72c65938
Matheus Biesdorf <matheusbiesdorf@hotmail.com>
2025-01-27 21:42:32 -0300

    Imagens

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import BarraLateral from "./componentes/BarraLateral";
 import Banner from "./componentes/Banner";
 import bannerBackground from "./assets/banner.png";
 import Galeria from "./componentes/Galeria";
+import fotos from './fotos.json'
+import { useState } from "react";
 
 const FundoGradiente = styled.div`
    background: linear-gradient(
@@ -34,7 +36,8 @@ const ConteudoGaleria = styled.section`
    flex-grow: 1;
 `;
 
-function App() {
+const App = () => {
+   const [fotosDaGaleria, setFotosDaGaleria] = useState(fotos)
    return (
       <FundoGradiente>
          <EstilosGlobais />
@@ -47,7 +50,7 @@ function App() {
                      texto="A galeria mais completa de fotos do espaço!"
                      backgroundImage={bannerBackground}
                   />
-                  <Galeria />
+                  <Galeria fotos={fotosDaGaleria}/>
                </ConteudoGaleria>
             </MainContainer>
          </AppContainer>
--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -1,32 +1,43 @@
-import styled from "styled-components";
-import Titulo from "../Titulo";
-import Tags from "./Tags";
-import Populares from "./Populares";
+import { styled } from "styled-components"
+import Titulo from "../Titulo"
+import Tags from "./Tags"
+import Populares from "./Populares"
+import Imagem from "./Imagem"
 
 const GaleriaContainer = styled.div`
     display: flex;
-
+    gap: 24px;
 `
 
 const SecaoFluida = styled.section`
     flex-grow: 1;
 `
 
-const Galeria = () => {
-   return (
-      <>
-         <Tags />
+const ImagensContainer = styled.section`
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    gap: 24px;
+`
 
-         <GaleriaContainer>
-            <SecaoFluida>
-               <Titulo>Navegue pela galeria</Titulo>
-            </SecaoFluida>
-            <Populares>
+const Galeria = ({ fotos = [] }) => {
+    return (
+        <>
+            <Tags />
+            <GaleriaContainer>
+                <SecaoFluida>
+                    <Titulo>Navegue pela galeria</Titulo>
+                    <ImagensContainer>
+                        {fotos.map(foto => <Imagem 
+                            key={foto.id} 
+                            foto={foto} />)
+                        }
+                    </ImagensContainer>
+                </SecaoFluida>
+                <Populares />
+            </GaleriaContainer>
+        </>
+    )
+}
 
-            </Populares>
-         </GaleriaContainer>
-      </>
-   );
-};
-
-export default Galeria;
+export default Galeria
